2f3e6fe09afd928c46cf5359e84f176a26240071
Nikhil Dubbaka <info@technikh.com>
2015-11-22 19:27:12 -0500

    Update ci_run_php_tests.sh

--- tests/scripts/ci_run_php_tests.sh
+++ tests/scripts/ci_run_php_tests.sh
@@ -2,7 +2,7 @@
 
 DB_URL=mysql://$DB_USERNAME@127.0.0.1/$DATABASE
 
-drush site-install testing --db-url=$DB_URL --site-name=my-site --yes
-drush en --yes simpletest
-drush runserver 127.0.0.1:8083 &
-drush test-run --uri=http://127.0.0.1:8083
+drush --root=../../docroot site-install testing --db-url=$DB_URL --site-name=my-site --yes
+drush --root=../../docroot en --yes simpletest
+drush --root=../../docroot runserver 127.0.0.1:8083 &
+drush --root=../../docroot test-run --uri=http://127.0.0.1:8083
