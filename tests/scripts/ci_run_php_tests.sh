a438d2c206fc1241f21409bb2a6e93be2717bda8
Nikhil Dubbaka <info@technikh.com>
2015-11-22 19:41:38 -0500

    Update ci_run_php_tests.sh

--- tests/scripts/ci_run_php_tests.sh
+++ tests/scripts/ci_run_php_tests.sh
@@ -7,4 +7,4 @@ cd ../../docroot
 drush site-install testing --db-url=$DB_URL --site-name=my-site --yes
 drush en --yes simpletest
 drush runserver 127.0.0.1:8083 &
-drush docroot test-run --uri=http://127.0.0.1:8083
+#drush docroot test-run --uri=http://127.0.0.1:8083
